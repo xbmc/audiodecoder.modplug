44dce2ff677d3080de37731a99574b82cfe3957f
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2014-10-31 15:23:19 +0100

    rebranding. xbmc -> kodi

--- src/ModplugCodec.cpp
+++ src/ModplugCodec.cpp
@@ -18,12 +18,12 @@
  *
  */
 
-#include "xbmc/libXBMC_addon.h"
+#include "kodi/libXBMC_addon.h"
 
 extern "C" {
 #include <libmodplug/modplug.h>
-#include "xbmc/xbmc_audiodec_dll.h"
-#include "xbmc/AEChannelData.h"
+#include "kodi/kodi_audiodec_dll.h"
+#include "kodi/AEChannelData.h"
 
 ADDON::CHelper_libXBMC_addon *XBMC           = NULL;
 
