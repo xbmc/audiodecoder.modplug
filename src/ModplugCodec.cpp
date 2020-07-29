565b2e432e84e1faa61ba848ada4e36994d2bb7f
Alwin Esch <alwin.esch@web.de>
2020-07-29 13:30:55 +0200

    clang format and code cleanup

--- src/ModplugCodec.cpp
+++ src/ModplugCodec.cpp
@@ -9,91 +9,92 @@
 //! \author Arne Morten Kvarving
 //! \brief Modplug audio decoder for Kodi
 
-#include <kodi/addon-instance/AudioDecoder.h>
+#include "ModplugCodec.h"
+
 #include <kodi/Filesystem.h>
 
-extern "C" {
-#include <libmodplug/modplug.h>
+CModplugCodec::CModplugCodec(KODI_HANDLE instance, const std::string& version)
+  : CInstanceAudioDecoder(instance, version)
+{
 }
 
-class ATTRIBUTE_HIDDEN CModplugCodec : public kodi::addon::CInstanceAudioDecoder
+CModplugCodec::~CModplugCodec()
 {
-public:
-  CModplugCodec(KODI_HANDLE instance, const std::string& version) :
-    CInstanceAudioDecoder(instance, version) {}
+  if (m_module)
+    ModPlug_Unload(m_module);
+}
 
-  ~CModplugCodec() override
-  {
-    if (m_module)
-      ModPlug_Unload(m_module);
-  }
+bool CModplugCodec::Init(const std::string& filename,
+                         unsigned int filecache,
+                         int& channels,
+                         int& samplerate,
+                         int& bitspersample,
+                         int64_t& totaltime,
+                         int& bitrate,
+                         AudioEngineDataFormat& format,
+                         std::vector<AudioEngineChannel>& channellist)
+{
+  kodi::vfs::CFile file;
+  if (!file.OpenFile(filename, 0))
+    return false;
 
-  bool Init(const std::string& filename, unsigned int filecache,
-            int& channels, int& samplerate,
-            int& bitspersample, int64_t& totaltime,
-            int& bitrate, AudioEngineDataFormat& format,
-            std::vector<AudioEngineChannel>& channellist) override
-  {
-    kodi::vfs::CFile file;
-    if (!file.OpenFile(filename,0))
-      return false;
+  int len = file.GetLength();
+  char* data = new char[len];
+  if (!data)
+    return false;
 
-    int len = file.GetLength();
-    char *data = new char[len];
-    if (!data)
-      return false;
+  file.Read(data, len);
+  file.Close();
 
-    file.Read(data, len);
-    file.Close();
+  // Now load the m_module
+  m_module = ModPlug_Load(data, len);
+  delete[] data;
 
-    // Now load the m_module
-    m_module = ModPlug_Load(data, len);
-    delete[] data;
+  if (!m_module)
+    return false;
 
-    if (!m_module)
-      return false;
+  channels = 2;
+  samplerate = 44100;
+  bitspersample = 16;
+  totaltime = (int64_t)ModPlug_GetLength(m_module);
+  format = AUDIOENGINE_FMT_S16NE;
+  channellist = {AUDIOENGINE_CH_FL, AUDIOENGINE_CH_FR};
+  bitrate, ModPlug_NumChannels(m_module);
 
-    channels = 2;
-    samplerate = 44100;
-    bitspersample = 16;
-    totaltime = (int64_t)ModPlug_GetLength(m_module);
-    format = AUDIOENGINE_FMT_S16NE;
-    channellist = { AUDIOENGINE_CH_FL, AUDIOENGINE_CH_FR };
-    bitrate, ModPlug_NumChannels(m_module);
+  return true;
+}
 
-    return true;
-  }
-
-  int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
-  {
-    if (!m_module)
-      return 1;
-
-    if ((actualsize = ModPlug_Read(m_module, buffer, size)) == size)
-      return 0;
-  
+int CModplugCodec::ReadPCM(uint8_t* buffer, int size, int& actualsize)
+{
+  if (!m_module)
     return 1;
-  }
 
-  int64_t Seek(int64_t time) override
-  {
-    if (!m_module)
-      return -1;
+  if ((actualsize = ModPlug_Read(m_module, buffer, size)) == size)
+    return 0;
 
-    ModPlug_Seek(m_module, (int)time);
-    return time;
-  }
+  return 1;
+}
 
-private:
-  ModPlugFile* m_module = nullptr;
-};
+int64_t CModplugCodec::Seek(int64_t time)
+{
+  if (!m_module)
+    return -1;
 
+  ModPlug_Seek(m_module, (int)time);
+  return time;
+}
+
+//------------------------------------------------------------------------------
 
 class ATTRIBUTE_HIDDEN CMyAddon : public kodi::addon::CAddonBase
 {
 public:
   CMyAddon() = default;
-  ADDON_STATUS CreateInstance(int instanceType, const std::string& instanceID, KODI_HANDLE instance, const std::string& version, KODI_HANDLE& addonInstance) override
+  ADDON_STATUS CreateInstance(int instanceType,
+                              const std::string& instanceID,
+                              KODI_HANDLE instance,
+                              const std::string& version,
+                              KODI_HANDLE& addonInstance) override
   {
     addonInstance = new CModplugCodec(instance, version);
     return ADDON_STATUS_OK;
@@ -101,5 +102,4 @@ public:
   ~CMyAddon() override = default;
 };
 
-
-ADDONCREATOR(CMyAddon);
+ADDONCREATOR(CMyAddon)
